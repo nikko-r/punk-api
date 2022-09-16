d8cf3e2d1892cab5ad712adee5d627caa97d4ba7
nikko <nikkorivas@outlook.com>
2022-09-16 15:06:47 +0100

    removed sidebar toggle button Fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,9 +4,9 @@ const NavBar = ({ searchBoxInput, handleInput }) => {
   return (
     <div className="navbar">
       <div className="navbar__toggle-sidebar">
+        {/* <div className="toggle-sidebar__line"></div>
         <div className="toggle-sidebar__line"></div>
-        <div className="toggle-sidebar__line"></div>
-        <div className="toggle-sidebar__line"></div>
+        <div className="toggle-sidebar__line"></div> */}
       </div>
       <input
         className="navbar__searchbar"
